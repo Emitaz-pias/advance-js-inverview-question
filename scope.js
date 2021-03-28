347b9df2b52a7b0bf063c6bde227c162c402b6d2
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-03-28 16:39:12 +0600

    scope finished

---
+++ scope.js
@@ -0,0 +1,10 @@
+// const let will only inside the local scope
+// var will global scope
+// read  before assinging var will get undefined let const will give erron
+
+add =(num2,num3)=>{
+
+    let name ="ream"
+}
+console.log(name);
+var nams = "goru"
