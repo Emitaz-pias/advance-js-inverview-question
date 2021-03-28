7bce7ac8aa297a2d521db2d6df6a219ab75151d1
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-03-28 13:47:32 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,10 @@
+// falsy : 0 ,undefined,null,"",false,NaN
+// truthy: -1,1," ","false",[],{} etc
+var age = 1;
+
+if( age){
+console.log("condition is true")
+}
+else{
+console.log("condition is false")
+}
